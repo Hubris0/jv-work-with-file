85c837901a74258518954be7954741ee07062272
user <pawel.m.janik@gmail.com>
2026-07-03 10:34:59 +0200

    Separated logic from getStatistic method to readFromFile and writeToFile.

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -5,20 +5,29 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class WorkWithFile {
+
     public void getStatistic(String fromFileName, String toFileName) {
-        int buyCount = 0;
+        String report = readFromFile(fromFileName);
+        writeToFile(report, toFileName);
+    }
+
+    public String readFromFile(String fileName) {
         int supplyCount = 0;
-        int operationIndex = 0;
-        int countIndex = 1;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+        int buyCount = 0;
+        String DELIMITER = ",";
+        int OPERATION_INDEX = 0;
+        int COUNT_INDEX = 1;
+        String OPERATION_BUY = "buy";
+        String OPERATION_SUPPLY = "supply";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] parts = line.split(",");
-                String operation = parts[operationIndex];
-                int count = Integer.parseInt(parts[countIndex]);
-                if (operation.equals("buy")) {
+                String[] parts = line.split(DELIMITER);
+                String operation = parts[OPERATION_INDEX];
+                int count = Integer.parseInt(parts[COUNT_INDEX]);
+                if (operation.equals(OPERATION_BUY)) {
                     buyCount += count;
-                } else if (operation.equals("supply")) {
+                } else if (operation.equals(OPERATION_SUPPLY)) {
                     supplyCount += count;
                 }
                 line = reader.readLine();
@@ -27,11 +36,14 @@ public class WorkWithFile {
             throw new RuntimeException("File can't be read", e);
         }
         int result = supplyCount - buyCount;
-        String resultString = "supply," + supplyCount + System.lineSeparator()
+        return "supply," + supplyCount + System.lineSeparator()
                 + "buy," + buyCount + System.lineSeparator()
                 + "result," + result;
+    }
+
+    public void writeToFile(String report, String toFileName) {
         try (java.io.FileWriter writer = new java.io.FileWriter(toFileName)) {
-            writer.write(resultString);
+            writer.write(report);
         } catch (IOException e) {
             throw new RuntimeException("File can't be written", e);
         }
